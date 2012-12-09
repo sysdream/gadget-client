d580b65e491b4be829355bd03a9d7525a7d3fb0f
Pierre Jaury <pierre@jaury.eu>
2012-12-09 15:09:35 +0100

    First stage development of the mapping module
    Object and Method objects now written, need to be tested

--- gadget/mapping.py
+++ gadget/mapping.py
@@ -61,16 +61,19 @@ class Registry(object):
     # the registry itself
     mappings = dict()
 
-    def register(cls, clazz, mapping):
+    @classmethod
+    def register(cls, classname):
         """
-        Register a new mapping
+        Register a new mapping, class decorator
 
         Keyword arguments:
-        clazz   -- the mapped Python class
-        mapping -- the mapped Java class name
+        classname -- the mapped Java class name
         """
-        cls.mappings[mapping] = clazz
+        def decorator(clazz):
+            cls.mappings[classname] = clazz
+        return decorator
 
+    @classmethod
     def resolve(cls, classnames):
         """
         Resolve a mapping
@@ -143,22 +146,103 @@ class Object(object):
     # global entry point cache
     _entry_points = []
 
-    def __init__(self, entry_point, path=[]):
+    def __init__(self, service, entry_point, path=[]):
         """
         Initialize the object
 
         Prepare the cache and save the object access properties
 
         Keyword arguments:
+        service     -- the inspection service
         entry_point -- the entry point number on the application side
         path        -- the path on the application side
         """
+        self._service = service
         self._entry_point = entry_point
         self._modifiers = modifiers
         self._path = path
         self._field_cache = None
         self._method_cache = None
-        self._class_cache = None
+
+    def __getattr__(self, name):
+        """
+        Access an attribute of the current object
+
+        Attributes may be fields, methods or even inner classes.
+        Resolution order is:
+        * fields (no matter if public or private)
+        * methods (no matter their visibility either)
+        * classes
+
+        Cache is refreshed using lazy mechanisms, ie. the method cache
+        will not be refreshed if a field matches the attribute name.
+        Methods returned as attributes are virtual methods: virtual method
+        resolution will be performed on the Java side whenever the method
+        object is called.
+        """
+        field = self._getfield(name)
+        if field is not None:
+            return field
+        method = self._getmethod(name)
+        if method is not None:
+            return method
+        clazz = self._getclass(name)
+        if clazz is not None:
+            return clazz
+        raise AttributeError("Unknown attribute %s" % name)
+
+    def _getfield(self, name):
+        """
+        Access a field of the current object given its name
+
+        There is no virtual field resolution mechanism in Java, so cache is
+        simply refreshed and the matching field is returned if available.
+        The cache is refreshed in two steps:
+        * the list of fields is stored as keys (the value is None)
+        * the field Object is generated when needed
+        """
+        # if the field list needs to be refreshed
+        if self._field_cache == None:
+            self._field_cache = self._service.get_fields(
+                self._entry_point, self._path)
+        # if the attribute is a field
+        if name in self._field_cache:
+            # if the specific field needs to be refreshed
+            modifiers, field = self._field_cache[name]
+            if type(field) is int:
+                field = self._service.get_field(
+                    self._entry_point, self._path, field)
+            return field
+
+    def _getmethod(self, name):
+        """
+        Access a method of the current object given its name
+
+        The method returned is a virtual method instance, ie. virtual method
+        resolution will be performed at call time on the Java code side.
+        """
+        # if the method list needs to be refreshed
+        if self._method_cache == None:
+            self._method_cache = self._service.get_methods(
+                self._entry_point, self._peth)
+        # if the attribute is a method
+        if name in self._method_cache:
+            # virtual method is simply a Method object with a string method
+            # instead of integer method id
+            return Method(self._service, self._entry_point, self._path, name)
+
+    def _getentrypoint(self):
+        """
+        Get an entry point for the current object
+
+        If the object is already an entry point, just return the entry point
+        value. Otherwise, it is necessary to first push the object.
+        """
+        if len(self._path) > 0
+            self._entry_point = self._service.push(
+                self._entry_point, self._path)
+            self._path = []
+        return self._entry_point
 
 
 class Class(object):
@@ -188,3 +272,44 @@ class Method(object):
     Simply holds a method caracteristics and provides abstraction for
     method invocation.
     """
+
+    def __init__(self, service, entry_point, path, method):
+        """
+        Initialize the method object
+
+        If the method is a method name (as string), then the method object
+        behaves like a virtual method and virtual method resolution is
+        performed when invoked. If the method is a method identifier
+        (as integer), then the method object behaves like a concrete method.
+
+        Keyword arguments:
+        service     -- the inspection service
+        entry_point -- the entry point number on the application side
+        path        -- the path on the application side
+        method      -- represented method
+        """
+        self._service = service
+        self._entry_point = entry_point
+        self._path = path
+        self._method = method
+
+    def __call__(self, *args):
+        """
+        Invoke the current method object with the given arguments
+
+        Arguments may be Object instances or base types that will be
+        cast as Object instances before actual method invocation.
+        """
+        # list of actual sent arguments
+        objects = [arg if isinstance(arg, Object)
+                   else self._service.to_object(arg)]
+        arguments = [arg._getentrypoint for arg in objects]
+        # if the method is a virtual method
+        if type(self._method is str):
+            entry_point = self._service.virtual(
+                self._entry_point, self._path, self._method, arguments)
+        else:
+            entry_point = self._service.invoke(
+                self._entry_point, self._path, self._method, arguments)
+        # return the result wrapped in an Object instance
+        return Object(self._service, entry_point)
